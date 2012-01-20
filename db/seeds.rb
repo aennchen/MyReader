cb9dfa02a6f5a8e639d8eaf291f27f2a31594944
Anne Schanz <write@anneschanz.de>
2012-01-19 23:08:29 -0800

    added sources and categories to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,42 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-c = Category.create do |c|
+n = Category.create do |c|
 	c.name = 'News'
+end
+
+t = Category.create do |c|
+	c.name = 'Tech'
+end
+
+p = Category.create do |c|
+	c.name = 'Politics'
 end
 
 Source.create do |s| 
 	s.title = 'tagesschau'
 	s.url = 'http://www.tagesschau.de/xml/rss2'
-	s.category = c
+	s.category = n
 end
 
 Source.create do |s|
 	s.title = 'taz'
 	s.url = 'http://www.taz.de/rss.xml'
-	s.category = c
+	s.category = n
 end
+
+Source.create do |s|
+	s.title = 'Netzpolitik'
+	s.url = 'http://netzpolitik.org/feed/'
+	s.category = t
+end
+
+Source.create do |s|
+	s.title = 'USA erklaert'
+	s.url = 'http://usaerklaert.wordpress.com/feed/'
+	s.category = p
+end
+
 
 # Create a default user
 User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
